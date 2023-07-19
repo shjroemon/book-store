62c157cec0c5914e4f5f5a3f522be82ac064678f
shjroemon <zzdaotuanhuyzz@gmail.com>
2023-07-19 18:03:31 +0700

    book-store

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,8 @@
-import { configureStore } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
+import { configureStore } from "@reduxjs/toolkit";
+import bookReducer from "../components/book/bookSlice.js";
 
 export const store = configureStore({
   reducer: {
-    counter: counterReducer,
+    book: bookReducer,
   },
 });
